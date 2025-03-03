d8ec419dffedf0602f6572cacfc71da0951078d2
Jérémie <jeremie.lohyer@alumni.univ-avignon.fr>
2025-03-03 16:41:29 +0100

    ajout gestion date check

--- DataLoader.py
+++ DataLoader.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 
 import pandas as pd
 
@@ -8,13 +8,16 @@ class DataLoader:
     src_path = "data/"
     feature_names = ['CDSEXE', 'MTREV', 'NBENF', 'CDSITFAM', 'CDTMT', 'CDDEM', 'CDMOTDEM', 'CDCATCL']
 
+    def error(self, data, text):
+        return 'ID : ' + str(data['ID']) + ' ' + text
+
     def check_feature(self, data, feature_type):
 
         if feature_type == 'CDSEXE':
             sexe = data[feature_type]
             if sexe == 2 or sexe == 3 or sexe == 4:
                 return None
-            return 'ID : ' + str(data['ID']) + ' sexe inconnu : ' + str(sexe)
+            return self.error(data, 'sexe inconnu : ' + str(sexe))
 
 
         elif feature_type == 'MTREV':
@@ -23,7 +26,7 @@ class DataLoader:
                 return None
             # elif mtrev > 0 and mtrev < 2000000: # 4 clients gros revenu et 1 > 1 000 000
             #     return None
-            return 'ID : ' + str(data['ID']) + ' revenu inconnu : ' + str(mtrev)
+            return self.error(data, 'revenu inconnu : ' + str(mtrev))
 
 
         elif feature_type == 'NBENF':
@@ -32,7 +35,7 @@ class DataLoader:
                 return None
             # if nbenf >= 0  and nbenf < 7: # 1 avec 6 enfants
             #     return None
-            return "ID : " + str(data['ID']) + " beaucoup d'enfants : " + str(nbenf)
+            return self.error(data, "beaucoup d'enfants : " + str(nbenf))
 
 
         elif feature_type == 'CDSITFAM':
@@ -41,7 +44,7 @@ class DataLoader:
                     cdsitfam == 'D' or cdsitfam == 'C' or cdsitfam == 'U' or cdsitfam == 'P' or \
                     cdsitfam == 'G' or cdsitfam == 'E' or cdsitfam == 'S' or cdsitfam == 'F':
                 return None
-            return 'ID : ' + str(data['ID']) + ' sitfam inconnu : ' + cdsitfam
+            return self.error(data, 'sitfam inconnu : ' + cdsitfam)
 
 
         elif feature_type == 'CDTMT':
@@ -50,7 +53,7 @@ class DataLoader:
                 return None
             # if cdtmt == 6: # 1 code societaire 6
             #     return None
-            return 'ID : ' + str(data['ID']) + ' statut societaire inconnu : ' + str(cdtmt)
+            return self.error(data, 'statut societaire inconnu : ' + str(cdtmt))
 
 
         elif feature_type == 'CDDEM':
@@ -59,14 +62,14 @@ class DataLoader:
                 return None
             # if cddem == 1:# 4 code demission 1
             #     return None
-            return 'ID : ' + str(data['ID']) + ' code demission inconnu : ' + str(cddem)
+            return self.error(data, 'code demission inconnu : ' + str(cddem))
 
 
         elif feature_type == 'CDMOTDEM':
             cdmotdem = data[feature_type]
             if cdmotdem == 'DV' or cdmotdem == 'RA' or cdmotdem == 'DA':
                 return None
-            return 'ID : ' + str(data['ID']) + ' motif demission inconnu : ' + cdmotdem
+            return self.error(data, 'motif demission inconnu : ' + cdmotdem)
 
 
         elif feature_type == 'CDCATCL':
@@ -75,16 +78,74 @@ class DataLoader:
                 return None
             # if cdcatcl == 22 or cdcatcl == 32: # 3 code 22 et 2 code 32
             #     return None
-            return 'ID : ' + str(data['ID']) + ' code categorie client inconnue : ' + str(cdcatcl)
+            return self.error(data, 'code categorie client inconnue : ' + str(cdcatcl))
 
         return None
 
-    def check_DATES(self, dtadh, datedem, anneedem, agead, rangagead, agedem, rangagedem, rangdem, adh, rangadh):
+    def getrange(self, s_range: str):
+        if type(s_range) != str:
+            return 0, range(0,1)
+        t_range = s_range.split('  ')
+        index = int(t_range[0], 16)
+        rg = t_range[1].split('-')
+        if rg[1] == '+':
+            return index, range(int(rg[0]), int(rg[0]) + 1000)
+        return index, range(int(rg[0]),int(rg[1]) + 1)
+
+    def date(self, sdt: str):
+        return datetime.strptime(sdt, "%d/%m/%Y").date()
+
+    def getannee(self, s_range: str):
+        t_range = s_range.split('  ')
+        return int(t_range[0]), int(t_range[1])
+
+    def check_DATES(self, data, sdtadh, sdtdem, sanneedem, agead, rangagead, agedem, rangagedem, rangdem, adh, rangadh):
+        # ----- etape 1 ----- vérification de l'age du client :
+
+        # age lors de l'adhesion
+        indice_range_age_adhesion, range_age_adhesion = self.getrange(rangagead)
+        if indice_range_age_adhesion < 1 or indice_range_age_adhesion > 8 :
+            return self.error(data, 'identifiant tranche age client adhesion inconnue : ' + str(rangagead))
+        if not agead in range_age_adhesion:
+            return self.error(data, 'indice tranche age client adhesion incorrect : ' + str(agead) + ' VS ' + str(rangagead))
+
+        # age lors de la demission
+        indice_range_age_demission, range_age_demission = self.getrange(rangagedem)
+        if indice_range_age_demission < 1 or indice_range_age_demission > 11:
+            return self.error(data, 'identifiant tranche age client demission inconnue : ' + str(rangagedem))
+        if not agedem in range_age_demission:
+            return self.error(data, 'indice tranche age client demission incorrect : ' +str(agedem) + ' VS ' + str(rangagedem))
+
+        if agedem < agead:
+            return self.error(data, "age de démission inférieure a l'age d'adhesion : " + str(agedem) + ' VS ' + str(agead))
+
+        # ----- etape 2 ----- vérification des dates d'adhesion/demission
+
+        # conversion des données spéciales
+        dtadh = self.date(sdtadh)
+        dtdem = self.date(sdtdem)
+        anneedem = int(sanneedem)
+
+        # durée d'adhesion
+        indice_range_duree_adhesion, range_duree_adhesion = self.getrange(rangadh)
+        if indice_range_duree_adhesion < 0 or indice_range_duree_adhesion > 7:
+            return self.error(data, 'indice duree adhesion inconnue : ' + str(rangadh))
+        if not adh in range_duree_adhesion:
+            return self.error(data, 'duree adhesion incorrecte : ' + str(adh) + ' VS ' + str(rangadh))
+
+        if dtdem < dtadh:
+            return self.error(data, "date de demission antérieure a la date d'adhesion " + str(dtdem) + ' VS ' + str(dtadh))
+
+        # année de démission
+        indice_annee_dem, annee_dem = self.getannee(rangdem)
+
+        if indice_annee_dem < 0 or indice_annee_dem > 8:
+            return self.error(data, 'indice annee demission inconnue : ' + str(rangdem))
+
+        if anneedem != annee_dem:
+            return self.error(data, 'annee de demission incorrecte : ' + str(anneedem) + ' VS ' + str(annee_dem))
+
         return None
-        return "date adhesion incorrecte : " + dtadh
-        return "date demission incorrecte : " + datedem
-        return "annee demission incorrecte : " + anneedem
-        return 'age adhesion incorrect : ' + agead
 
     def checkData(self, data):
 
@@ -95,7 +156,7 @@ class DataLoader:
                 e = self.check_feature(row, type)
                 if e:
                     print(e)
-            e = self.check_DATES(row['DTADH'],row['DTDEM'],row['ANNEEDEM'],row['AGEAD'],row['RANGAGEAD'],
+            e = self.check_DATES(row, row['DTADH'],row['DTDEM'],row['ANNEEDEM'],row['AGEAD'],row['RANGAGEAD'],
                                  row['AGEDEM'],row['RANGAGEDEM'],row['RANGDEM'],row['ADH'],row['RANGADH'])
             if e:
                 print(e)
@@ -115,6 +176,7 @@ class DataLoader:
         # print feature types
         print(data.dtypes)
         return data
+
 
 
 if __name__ == '__main__':
